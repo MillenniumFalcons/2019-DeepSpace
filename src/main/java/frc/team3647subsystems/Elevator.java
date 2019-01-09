7835846df1bf0dbf2d4a357d8f43a60acd77a89d
Surya <suryamadhan@gmail.com>
2019-01-09 14:25:13 -0800

    Create Elevator.java

---
+++ src/main/java/frc/team3647subsystems/Elevator.java
@@ -0,0 +1,32 @@
+package frc.team3647subsystems;
+
+import frc.robot.*;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.FollowerType;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+public class Elevator
+{
+    public static void elevatorInitialization()
+	{
+        //Config PID for Motors
+    }
+
+    public static void moveElevatorPosition(double position)
+    {
+        //Motion Magic
+    }
+
+    public static void stopElevator()
+    {
+        //Stop Elevator
+    }
+
+    public static void moveElevator(double speed)
+    {
+        // Percent Output
+    }
+
+}
