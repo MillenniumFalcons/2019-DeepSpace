2c1aa695cce527d72952e26706a0f5701fe8b5c1
Kunal Singla <tekunalogy@gmail.com>
2019-02-28 09:32:53 -0800

    clean up code

--- DeepSpaceNew/src/main/java/frc/robot/Robot.java
+++ DeepSpaceNew/src/main/java/frc/robot/Robot.java
@@ -7,16 +7,9 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import frc.team3647autonomous.AutonomousSequences;
-import frc.team3647autonomous.DriveSignal;
-import frc.team3647autonomous.MotionProfileDirection;
 import frc.team3647autonomous.Odometry;
-import frc.team3647autonomous.RamseteFollower;
-import frc.team3647autonomous.TrajectoryUtil;
 import frc.team3647inputs.*;
 import frc.team3647subsystems.*;
-import frc.team3647subsystems.Elevator.ElevatorLevel;
-import frc.team3647utility.Units;
-import jaci.pathfinder.Trajectory;
 
 public class Robot extends TimedRobot 
 {
@@ -41,9 +34,6 @@ public class Robot extends TimedRobot
 	@Override
 	public void robotInit() 
 	{
-		// m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-		// m_chooser.addOption("My Auto", kCustomAuto);
-		// SmartDashboard.putData("Auto choices", m_chooser);
 
 		mainController = new Joysticks(0);
 		coController = new Joysticks(1);
@@ -62,39 +52,23 @@ public class Robot extends TimedRobot
 		gyro.updateGyro();
 		updateJoysticks();
 	}
-
-	DriveSignal driveSignal;
-	Trajectory trajectory;
-	RamseteFollower ramseteFollower;
-	Trajectory.Segment current;
 	
 
 	@Override
 	public void autonomousInit() 
 	{
-    // The NEO takes the Motor-output in percent from the SRX and since SRX values are using motion-magic, it "follows" the SRX
+    	// The NEO takes the Motor-output in percent from the SRX and since SRX values are using motion-magic, it "follows" the SRX
 		autoNotifier = new Notifier(()->
 		{
 			Arm.armNEOFollow();
-      Odometry.getInstance().runOdometry();
-      
-    });
-    autoNotifier.startPeriodic(0.01);
+      		Odometry.getInstance().runOdometry();
+    	});
+    	autoNotifier.startPeriodic(0.01);
 
 		gyro.resetAngle();
 		Drivetrain.drivetrainInitialization();
 		Drivetrain.resetEncoders();
 		AirCompressor.runCompressor();
-		// SeriesStateMachine.seriesStateMachineInit();
-		// Arm.armInitialization();
-		// Elevator.elevatorInitialization();
-		// driveSignal = new DriveSignal();
-		// trajectory = TrajectoryUtil.getTrajectoryFromName("Level2RightToCargoRight");
-		// ramseteFollower = new RamseteFollower(trajectory,
-		// MotionProfileDirection.FORWARD);
-		// Odometry.getInstance().setInitialOdometry(trajectory);
-		// Odometry.getInstance().odometryInit();
-		// ranBackwardsOnce = false;
 		AutonomousSequences.autoInit("Level2RightToCargoRight");
 		autoTimer.reset();
 		autoTimer.start();
@@ -112,15 +86,6 @@ public class Robot extends TimedRobot
 			// Arm.runArm();
 		}
 		AutonomousSequences.level2RightToCargoShipRight();
-		// driveSignal = ramseteFollower.getNextDriveSignal();
-		// current = ramseteFollower.currentSegment();
-
-		// right = Units.metersToEncoderTicks(driveSignal.getRight() / 20);
-		// left = Units.metersToEncoderTicks(driveSignal.getLeft() / 20);
-		// Drivetrain.setAutoVelocity(left, right);
-		// System.out.println("Left Vel: " + (driveSignal.getLeft()) + "\nRight Vel: " +
-		// (driveSignal.getRight()));
-		// System.out.println("Left ticks Vel: " + (left) + "\nRight Vel: " + (right));
 	}
 
 	@Override
@@ -131,13 +96,14 @@ public class Robot extends TimedRobot
 		Elevator.elevatorInitialization();
 		SeriesStateMachine.seriesStateMachineInit();
 		ShoppingCart.shoppingCartInit();
-    Drivetrain.drivetrainInitialization();
+    	Drivetrain.drivetrainInitialization();
 
-    teleopNotifier = new Notifier(()->
+		teleopNotifier = new Notifier(() ->
 		{
 			Arm.armNEOFollow();
-      
-    });teleopNotifier.startPeriodic(.01);
+		});
+		teleopNotifier.startPeriodic(.01);
+
 	}
 
 	@Override
@@ -149,29 +115,29 @@ public class Robot extends TimedRobot
 		} 
 		else if (Arm.armEncoderValue > Constants.armSRXVerticalStowed && mainController.rightBumper) 
 		{
-      System.out.println("VISION MODE FOR BOTTOM LIMELIGHT");
+			System.out.println("VISION MODE FOR BOTTOM LIMELIGHT");
 			double joyY = mainController.leftJoyStickY;
 			AutonomousSequences.limelightBottom.visionTargetingMode();
-      AutonomousSequences.limelightBottom.center(0.1);
-      AutonomousSequences.limelightTop.driverFlipped();
+			AutonomousSequences.limelightBottom.center(0.1);
+			AutonomousSequences.limelightTop.driverFlipped();
 			double leftIn = AutonomousSequences.limelightBottom.leftSpeed + joyY;
 			double rightIn = AutonomousSequences.limelightBottom.rightSpeed + joyY;
 			Drivetrain.setPercentOutput(leftIn, rightIn);
 		} 
 		else if (Arm.armEncoderValue < Constants.armSRXVerticalStowed && mainController.rightBumper) 
 		{
-      System.out.println("VISION MODE FOR TOP LIMELIGHT");
+			System.out.println("VISION MODE FOR TOP LIMELIGHT");
 			double joyY = mainController.leftJoyStickY;
 			AutonomousSequences.limelightTop.visionTargetingMode();
-      AutonomousSequences.limelightTop.center(0.1);
-      AutonomousSequences.limelightBottom.driverFlipped();
+			AutonomousSequences.limelightTop.center(0.1);
+			AutonomousSequences.limelightBottom.driverFlipped();
 			double leftIn = AutonomousSequences.limelightTop.leftSpeed + joyY;
 			double rightIn = AutonomousSequences.limelightTop.rightSpeed + joyY;
 			Drivetrain.setPercentOutput(leftIn, rightIn);
     }
 		else 
 		{
-      System.out.println("BORK");
+			System.out.println("BORK");
 			AutonomousSequences.limelightBottom.driverMode();
 			AutonomousSequences.limelightTop.driverMode();
 			Drivetrain.customArcadeDrive(mainController.rightJoyStickX * .7, mainController.leftJoyStickY, gyro);
@@ -190,7 +156,7 @@ public class Robot extends TimedRobot
 	{
 		AutonomousSequences.limelightTop.disabledMode();
 		AutonomousSequences.limelightBottom.disabledMode();
-		// // TestFunctions.vController.disabledMode();
+		// TestFunctions.vController.disabledMode();
 		// Arm.disableArm();
 		// Drivetrain.setToCoast();
 		// Odometry.getInstance().closeOdoThread();
@@ -228,9 +194,9 @@ public class Robot extends TimedRobot
 		// Elevator.elevatorInitialization();
 		// Elevator.elevatorMaster.enableCurrentLimit(true);
 		// Elevator.elevatorMaster.configContinuousCurrentLimit(50);
-    // Arm.armInitialization()s;
+    	// Arm.armInitialization()s;
     
-    ShoppingCart.shoppingCartInit();
+		ShoppingCart.shoppingCartInit();
 	}
 
 	@Override
@@ -248,11 +214,11 @@ public class Robot extends TimedRobot
 		// Elevator.elevatorMaster.enableCurrentLimit(true);
 		// Drivetrain.customArcadeDrive(mainController.rightJoyStickX , mainController.leftJoyStickY, gyro);
 		// ShoppingCart.runShoppingCartSPX(mainController.leftJoyStickY);
-    // BallShooter.intakeMotor.set(ControlMode.PercentOutput, -.4);
-    // Elevator.setOpenLoop(mainController.rightJoyStickY);
-    ShoppingCart.setShoppinCartEncoder();
-    ShoppingCart.printPosition();
-    ShoppingCart.shoppingCartMotor.set(ControlMode.PercentOutput, mainController.leftJoyStickY);
+    	// BallShooter.intakeMotor.set(ControlMode.PercentOutput, -.4);
+		// Elevator.setOpenLoop(mainController.rightJoyStickY);
+		ShoppingCart.setShoppinCartEncoder();
+		ShoppingCart.printPosition();
+		ShoppingCart.shoppingCartMotor.set(ControlMode.PercentOutput, mainController.leftJoyStickY);
 	}
 
 	public void updateJoysticks()
