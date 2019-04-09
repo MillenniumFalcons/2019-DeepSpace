2835d186c4ffaae00e013f3f526091ee015c51e9
Kunal Singla <tekunalogy@gmail.com>
2019-04-08 21:10:39 -0700

    made code more efficient
    improved efficiency of code

--- DeepSpaceNew/src/main/java/frc/team3647autonomous/AutonomousSequences.java
+++ DeepSpaceNew/src/main/java/frc/team3647autonomous/AutonomousSequences.java
@@ -240,11 +240,6 @@ public class AutonomousSequences
 				break;
 		}
 	}
-	
-	public static void bruteForceAuto()
-	{
-		//lol u rlly thought i was going to make new autos? lmao
-	}
 
 	public static void sideCargoShipAuto()
 	{
